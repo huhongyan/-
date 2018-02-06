df2ed9383b5dfa9b6748a40a7ca77dc3249c8f55
huhongyan <huhongyan@xingxingcar.com>
2018-02-06 19:31:13 +0800

    测试代码

---
+++ client/pages/wish/wish.js
@@ -0,0 +1,12 @@
+//logs.js
+// const util = require('../../utils/util.js')
+import * as util from '../../utils/util.js'
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    
+  }
+})
