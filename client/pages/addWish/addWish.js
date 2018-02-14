4d3d0a18cd0e0bfe778d64e64391bb77ccc473bd
huhongyan <huhongyan@xingxingcar.com>
2018-02-14 11:13:55 +0800

    add wish

--- client/pages/addWish/addWish.js
+++ client/pages/addWish/addWish.js
@@ -1,66 +1,119 @@
 // pages/addWish/addWish.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+import * as util from '../../utils/util.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    // imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1518530090321&di=e06b698b1e47ad9819801990768035a0&imgtype=0&src=http%3A%2F%2Fb.hiphotos.baidu.com%2Fzhidao%2Fwh%253D450%252C600%2Fsign%3D3de411617dcb0a468577833d5e53da12%2F0b55b319ebc4b74572be448dc9fc1e178a821506.jpg',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+  },
+
+  formsubmit() {
+
+  },
+
+  // 上传图片接口
+  doUpload: function () {
+    var that = this
+
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        util.showBusy('正在上传')
+        var filePath = res.tempFilePaths[0]
+        // 上传图片
+        wx.uploadFile({
+          url: config.service.uploadUrl,
+          filePath: filePath,
+          name: 'file',
+          success: function (res) {
+            debugger
+            util.showSuccess('上传图片成功')
+            res = JSON.parse(res.data)
+            that.setData({
+              imgUrl: res.data.imgUrl
+            })
+          },
+          fail: function (e) {
+            util.showModel('上传图片失败')
+          }
+        })
+
+      },
+      fail: function (e) {
+        console.error(e)
+        util.showModel('上传图片失败', e)
+      }
+    })
+  },
+  // 预览图片
+  previewImg: function () {
+    wx.previewImage({
+      current: this.data.imgUrl,
+      urls: [this.data.imgUrl]
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
